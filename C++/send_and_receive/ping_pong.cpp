6ded9b2e22610cd1a4c6cc43bb94c44a9a447910
root <root@Acer.localdomain>
2019-11-08 08:41:06 +0100

    Ping pong - Commentaires

--- C++/send_and_receive/ping_pong.cpp
+++ C++/send_and_receive/ping_pong.cpp
@@ -5,35 +5,56 @@
 // free to modify it for your own use. Any distribution of the code must
 // either provide a link to www.mpitutorial.com or keep this header intact.
 //
-// Ping pong example with MPI_Send and MPI_Recv. Two processes ping pong a
-// number back and forth, incrementing it until it reaches a given value.
+// Un exemple  avec MPI_Send et MPI_Recv.
+// Deux processus se renvoient mutuellement un nombre, l'incrémentant
+// jusqu'à ce qu'il atteigne une certaine limite.
 //
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
+
+    // Le nombre d'échanges entre les processus.
     const int PING_PONG_LIMIT = 10;
 
-    // Initialize the MPI environment
+    // Initialise l'environnement d'exécution MPI.
     MPI_Init(NULL, NULL);
-    // Find out rank, size
+    
+    /*
+     * Détermine le rang donné du processus appelant dans
+     * le communicateur donné et donne le résultat à la variable myRank.
+     * 
+     * MPI_COMM_WORLD => le communicateur par défaut.
+     * Source: https://www.codingame.com/playgrounds/349/introduction-to-mpi/mpi_comm_world-size-and-ranks
+     */
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    /*
+     * Détermine le nombre de processus associés au
+     * communicateur donné et donne le résultat à la variable nbTask.
+     * 
+     * MPI_COMM_WORLD => le communicateur par défaut.
+     * Source: https://www.codingame.com/playgrounds/349/introduction-to-mpi/mpi_comm_world-size-and-ranks
+     */
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // We are assuming at least 2 processes for this task
+    // On s'attend à ce qu'il y ait au moins 2 processus.
     if (world_size != 2) {
         fprintf(stderr, "World size must be two for %s\n", argv[0]);
+
+        // Termine l'exécution de l'environnement MPI et retourne une erreur.
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
     int ping_pong_count = 0;
     int partner_rank = (world_rank + 1) % 2;
+
     while (ping_pong_count < PING_PONG_LIMIT) {
         if (world_rank == ping_pong_count % 2) {
-            // Increment the ping pong count before you send it
+            // Incrémente la valeur avant de l'envoyer.
             ping_pong_count++;
             MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
             printf("%d sent and incremented ping_pong_count %d to %d\n",
@@ -45,5 +66,6 @@ int main(int argc, char** argv) {
                     world_rank, ping_pong_count, partner_rank);
         }
     }
+    // Termine l'exécution de l'environnement MPI.
     MPI_Finalize();
 }
