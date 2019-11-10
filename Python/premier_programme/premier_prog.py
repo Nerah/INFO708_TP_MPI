4c659cadb7fb7cea16588398a8541e78b387ecd4
root <root@Acer.localdomain>
2019-11-10 23:02:07 +0100

    First_prog - Harmonisation entre les langages.

--- Python/premier_programme/premier_prog.py
+++ Python/premier_programme/premier_prog.py
@@ -1,10 +1,21 @@
 from mpi4py import MPI
 
 def main():
-    comm = MPI.COMM_WORLD  #comunicateur avec des valeurs par default
+
+    # MPI_COMM_WORLD => le communicateur par defaut.
+    comm = MPI.COMM_WORLD
+    
+    """
+     * Determine le rang donne du processus appelant dans
+     * le communicateur donne et donne le resultat a la variable myRank.
+    """
     rank = comm.Get_rank()
+    """
+     * Determine le nombre de processus associes au
+     * communicateur donne et donne le resultat a la variable nbTask.
+    """
     size = comm.Get_size()
-    print("hello from " + str(rank) + " in " + str(size))
+    print("I am task " + str(rank) + " out of " + str(size))
 
 
 if __name__ == '__main__':
